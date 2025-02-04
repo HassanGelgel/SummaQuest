046f3f03f17dbd60876c6288d7edfa38b1701dcb
HassanGelgel <hassangelgel555@gmail.com>
2025-02-04 15:00:29 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/summaquest/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.summaquest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+const backgroundColor = Color(0xffFEF9F1);
+const mainColor = Color(0xffAA8A78);
+const sideColor = Color(0xffF8BC83);
